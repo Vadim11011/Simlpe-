96bb1de4c4c288f4652d9e871ae475d0c5b1361e
Vadim00100 <takerprolm@gmail.com>
2024-07-02 19:21:37 +0300

    intinal comit

---
+++ search.js
@@ -0,0 +1,13 @@
+const input_div = document.getElementById("input_div");
+const search_btn = document.getElementById("search_btn");
+const reset_btn = document.getElementById("reset_btn")
+const input = document.getElementById("input") 
+
+search_btn.onclick = function(){
+    input_div.style.display = "inline";
+}
+
+reset_btn.onclick = function(){
+    input.value = "";
+    input_div.style.display = "none"
+}
